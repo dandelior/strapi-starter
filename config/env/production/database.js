f7b798b05cfcc7d8a4a4425ec1ab16089baebb0e
dandelior <jberrioscifuentes@gmail.com>
2022-06-18 19:37:38 -0400

    +

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "a13f599342f9c949ffa5  "),
+        password: env("DATABASE_PASSWORD", "a13f599342f9c949ffa5"),
       },
       options: {
         ssl: false,
