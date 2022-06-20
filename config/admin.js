ed57ddca7f9097e835a4f71b540a15b1f855f87a
dandelior <jberrioscifuentes@gmail.com>
2022-06-20 18:15:28 -0400

    setup for Cleavr

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "930e349ca7487d1544788aa9bfc67a41"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
 });
