4ac2b9c643e11c4c76e28eb94c47ac1c5f167479
dandelior <jberrioscifuentes@gmail.com>
2022-06-20 21:00:08 -0400

    +

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      database: env("DATABASE_NAME", "stra_db_vRk39lTB"),
+      user: env("DATABASE_USERNAME", "stra_user_ENbgMeSf"),
+      password: env("DATABASE_PASSWORD", "dgIo5bwztyZSMG61wfXaP"),
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
